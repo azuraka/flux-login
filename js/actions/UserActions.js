c02290331586dda5838025b653f4a217860d4498
Sahil Lamba <1sahillamba@gmail.com>
2016-05-07 16:05:01 +0530

    added login call

--- js/actions/UserActions.js
+++ js/actions/UserActions.js
@@ -34,11 +34,35 @@ var UserActions = {
     });
   },
 
+
+  // Input: {‘email’, ‘password’}
+  // 	Output: {‘status’, ‘message’, ‘data’: User details}
+
   UserLogin: function(email, passwd) {
-  	AppDispatcher.dispatch({
-      actionType: UserConstants.USER_LOGIN,
-      text: text
-    });
+
+
+      AppDispatcher.dispatch({
+        actionType: UserConstants.USER_LOGIN,
+        text: text
+      });
+
+        $.ajax({
+          type: 'POST',
+          url: "http://docx.8finatics.com/auth/login",
+          dataType: 'json',
+          cache: false,
+          headers: {
+            "X-Requested-With": "XMLHttpRequest"
+        },
+          success: function(data) {
+          	console.log("login ajax successful")
+            this.setState({data: data});
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+          }.bind(this)
+        });
+
   }
 };
 
